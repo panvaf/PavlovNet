a36fc3245c50089ed8ee14a70f9c0c159dd09de5
panvaf <pvafeidi@caltech.edu>
2021-05-19 00:38:42 -0700

    Arbitrary duration of CS

--- memory_net.py
+++ memory_net.py
@@ -7,20 +7,32 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import os
+from pathlib import Path
 
 # Constants
-n_neu = 64          # number of recurrent neurons
+n_neu = 128         # number of recurrent neurons
 n_batch = 100       # size of training batch
-n_iter = 5e3        # number of batches
+n_iter = 1e4        # number of batches
 n_in = 20           # size of patterns
-t_dur = 2           # duration of trial
-CS_dur = .5         # duration of CS presentation
+t_dur = 3           # duration of trial
+CS_mindur = .5      # minimum duration of CS presentation
+CS_maxdur = 2       # maximum duration of CS presentation
 dt = 1e-2           # step size
 tau = 1e-1          # neuronal time constant (synaptic+membrane)
 n_sd = .1           # standard deviation of injected noise
 print_every = int(n_iter/100)
-n_t = int(t_dur/dt); n_CS = int(CS_dur/dt); n_grace = 2*int(tau/dt)
+n_t = int(t_dur/dt); n_CS_mindur = int(CS_mindur/dt)
+n_CS_maxdur = int(CS_maxdur/dt); n_grace = 2*int(tau/dt)
 
+# Save location
+data_path = str(Path(os.getcwd()).parent) + '\\trained_networks\\'
+net_file = 'MemNet' + str(n_neu) + \
+            (('insz' + str(n_in)) if n_in != 20 else '') + \
+            (('tdur' + str(t_dur)) if t_dur != 2 else '') + \
+            (('n_iter' + str(n_iter)) if not n_iter==1e4 else '') + \
+            (('Noise' + str(n_sd)) if n_sd else '') + \
+            (('tau' + str(1e3*tau)) if tau != 1e-1 else '')
 
 # RNN class
 
@@ -107,11 +119,13 @@ optimizer = optim.Adam(net.parameters(), lr=0.001)
 running_loss = 0; k = 0
 train_loss = np.zeros(100)
 
-
 for i in range(int(n_iter)):
     # Generate data for current batch
+    n_CSs = np.random.randint(n_CS_mindur,n_CS_maxdur,n_batch)
     CS = torch.randint(low=0, high=2,size=(n_batch,1,n_in))
-    inputs = torch.zeros(n_batch,n_t,n_in); inputs[:,0:n_CS,:] = CS
+    inputs = CS.repeat(1,n_t,1).float()
+    for j, n_CS in enumerate(n_CSs):
+        inputs[j,n_CS:,:] = 0
     target = CS.repeat(1,n_t,1)
     mask = torch.ones(n_batch,n_t,n_in); mask[:,0:n_grace,:] = 0
 
@@ -129,3 +143,7 @@ for i in range(int(n_iter)):
         print('Loss {:0.3f}'.format(running_loss))
         train_loss[k] = running_loss
         running_loss = 0; k += 1
+
+# Save network
+torch.save({'state_dict': net.state_dict(),'train_loss': train_loss},
+                    data_path + net_file + '.pth')
